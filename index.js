1422d8fa2af29a85913249d5e5b901fa64f688a4
Raezarius <raezlan@gmail.com>
2023-06-07 12:43:16 +0800

    Update index.js
    Learning fundamentals

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
-function MyButton() {
-    return (
-      <button>
-        I'm a button
-      </button>
-    );
-  }
-
-root.render(<MyButton />);
+root.render(<p>Hello</p>);
