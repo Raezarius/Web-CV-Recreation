428ed8204f59c2d87d226c27487ed6082da51ee3
Raezarius <raezlan@gmail.com>
2023-06-07 12:57:03 +0800

    Update index.js
    Further study on fundamentals.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
-root.render(<p>Hello</p>);
+const element = <p>Hello</p>;
+
+root.render(element);
