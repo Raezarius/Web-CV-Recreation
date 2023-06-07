2388df626fddb6d9598cfa26442070f3dc0f2320
Raezarius <raezlan@gmail.com>
2023-06-07 14:15:49 +0800

    Recreating Resume through React.js
    Table method testing

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
-const element = <p>Hello</p>;
+const element = (
+    <table>
+    <tr>
+      <th>Wan Adam<br> Raezlan</br></th>
+      <td>Front-end Developer</td>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+  </table>
+);
 
 root.render(element);
