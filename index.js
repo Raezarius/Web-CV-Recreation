ec862e861b5e1a4e6bea0868eb51521ac59892ef
Raezarius <raezlan@gmail.com>
2023-06-08 10:43:51 +0800

    Update index.js
    Using <p> inside a <td>.

--- index.js
+++ index.js
@@ -5,21 +5,19 @@ const element = (
     <table>
         <thead>
             <tr>
-            <td>Wan Adam</td>
+            <td>
+                <p>Wan Adam Raezlan</p>
+                <p>Front-end Developer</p>
+            </td>
             <td>Wan Adam Raezlan bin Safian Aziz Kasim</td>
             </tr>
 
             <tr>
-            <td>Raezlan</td>
             <td>170, Jalan Sri Hartamas 2, Sri Hartamas,</td>
             <tr>50480, Kuala Lumpur</tr>
             <tr></tr>
             <tr>012-3923552</tr>
             <tr>raezlan@gmail.com</tr>
-            </tr>
-
-            <tr>
-            <td>Front-end Developer</td>
             </tr>
         </thead>
         <tbody>
